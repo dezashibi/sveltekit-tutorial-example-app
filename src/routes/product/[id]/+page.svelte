8dca2f2b5ffe7c50378e00265dd21ec95cfa20b7
Navid Dezashibi <dezashibi@gmail.com>
2024-02-15 13:44:58 +0400

    navigation and routing with route params

---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h1>Product Page:</h1>
+
+<p>Product ID: {$page.params.id}</p>
+
+<a href="/product/22333">Other Product</a>
+
+<style>
+</style>
