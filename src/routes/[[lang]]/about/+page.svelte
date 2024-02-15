925ae5ffe9a47d95a97d5a85c77d3474924b7cc2
Navid Dezashibi <dezashibi@gmail.com>
2024-02-15 14:15:44 +0400

    optional parameters

--- src/routes/[[lang]]/about/+page.svelte
+++ src/routes/[[lang]]/about/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	let name = 'Navid';
 </script>
 
 <h1>About {name}</h1>
 
+{JSON.stringify($page.params)}
+<br />
 <button
 	on:click={() => {
 		goto('/');
