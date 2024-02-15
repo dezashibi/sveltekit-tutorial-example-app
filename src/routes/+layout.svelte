37e3a17f1f51f57d4069b842214ced02d69d4152
Navid Dezashibi <dezashibi@gmail.com>
2024-02-15 15:01:01 +0400

    layouts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+<slot />
+
+<footer style:margin-top="60px">Made using Sveltkit.</footer>
---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/settings">Main Settings</a>
+	<a href="/settings/profile">Profile Settings</a>
+	<a href="/settings/general">General Settings</a>
+</nav>
+
+<slot />
