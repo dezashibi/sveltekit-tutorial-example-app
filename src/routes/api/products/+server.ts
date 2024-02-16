5c959756648f4e3970ce9528243de6d71397e143
Navid Dezashibi <dezashibi@gmail.com>
2024-02-16 09:34:25 +0400

    post request

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -1,5 +1,4 @@
-// import { error, json, type RequestHandler } from "@sveltejs/kit";
-import { json, type RequestHandler } from "@sveltejs/kit";
+import { error, json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
     const { products } = await (await import('$lib/dummy-products.json')).default;
@@ -19,4 +18,18 @@ export const GET: RequestHandler = async () => {
     // return new Response(JSON.stringify(products), {
     //     status: 200
     // });
+};
+
+export const POST: RequestHandler = async ({ request }) => {
+    const product = await request.json();
+
+    console.log(product);
+
+    if (!product.title) {
+        throw error(400, 'product title is required');
+    }
+
+    return json({ product, success: true }, {
+        status: 201
+    });
 }
