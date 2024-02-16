d571e8dcfd98f0c4fb46865546e75bc304c8128d
Navid Dezashibi <dezashibi@gmail.com>
2024-02-16 09:35:59 +0400

    comment

--- src/routes/products/+page.ts
+++ src/routes/products/+page.ts
@@ -1,6 +1,6 @@
 import type { PageData } from "./$types";
 
-// this will run after server only code
+// this will run after server only code (but will run on both server and the client)
 export const load: PageData = ({ data }: { data: PageData }) => {
     return data;
 };
