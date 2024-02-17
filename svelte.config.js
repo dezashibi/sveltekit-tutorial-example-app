af6c2406c111af3b055d530f53200883058f234b
Navid Dezashibi <dezashibi@gmail.com>
2024-02-17 11:49:47 +0400

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto'; 
-import nodeAdapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-auto';
+// import nodeAdapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,8 +12,8 @@ const config = {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        // adapter: adapter(),
-        adapter: nodeAdapter(),
+        adapter: adapter(),
+        // adapter: nodeAdapter(),
 
         // this wasn't here I've added to remember this is the way I can change the default folder configs
         files: {
